1d4a0eadd8448ea9b560abf218b10be90e336611
Dhruv garg <54364530+animedhruv@users.noreply.github.com>
2019-12-13 14:58:15 +0530

    added gci.c
    C code for finding the factorial of any integer

---
+++ gci.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+int a ,c;
+double b=1;
+printf("enter the number whose factorial is to be found");
+scanf("%d",&a);
+for(int n=1;n<=a;n++){
+b=b*n;
+
+}
+printf("factorial of given number is ");
+printf("%.0lf",b);
+}
